27511313a5b644023831d56114fb008a6c18a3d0
ThreeSixes <actual@joshlucy.com>
2015-01-22 17:16:16 -0500

    Added cprTest.py, a script that tests CPR position decoding. W00t.

---
+++ cprTest.py
@@ -0,0 +1,19 @@
+from cprMath import cprMath
+
+cprDecoder = cprMath()
+
+surface = False
+dataPoints = [
+	# Formatted in [[even lat, even lon], [odd lat, odd lon], last format]
+	[[92095, 39846], [88385, 125818], 1]
+]
+
+# Debug for our mental heath.
+#cprDecoder.debugToggle(True)
+
+for thisPoint in dataPoints:
+	coords = cprDecoder.decodeCPR(thisPoint[0], thisPoint[1], thisPoint[2], surface)
+	if coords != False:
+		print str(coords[0]) + ", " + str(coords[1])
+	else:
+		print "Bad data."

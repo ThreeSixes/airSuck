ac14b8648c1ddab4e29157eaf9178aceca3416a8
ThreeSixes <actual@joshlucy.com>
2015-01-26 21:14:00 -0500

    Added a new test coordinate and turned on debugging by default

--- cprTest.py
+++ cprTest.py
@@ -5,11 +5,12 @@ cprDecoder = cprMath()
 surface = False
 dataPoints = [
 	# Formatted in [[even lat, even lon], [odd lat, odd lon], last format]
-	[[92095, 39846], [88385, 125818], 1]
+	[[92095, 39846], [88385, 125818], 1],
+	[[26825, 26641], [36192, 128497], 1]
 ]
 
 # Debug for our mental heath.
-#cprDecoder.debugToggle(True)
+cprDecoder.debugToggle(True)
 
 for thisPoint in dataPoints:
 	coords = cprDecoder.decodeCPR(thisPoint[0], thisPoint[1], thisPoint[2], surface)

573824357531eea2c1d8d1e3486acd1a9e1574ec
ThreeSixes <actual@joshlucy.com>
2015-01-30 17:47:12 -0500

    Added new test coords.

--- cprTest.py
+++ cprTest.py
@@ -6,7 +6,7 @@ surface = False
 dataPoints = [
 	# Formatted in [[even lat, even lon], [odd lat, odd lon], last format]
 	[[92095, 39846], [88385, 125818], 1], # This decodes correctly, from http://www.lll.lu/~edward/edward/adsb/DecodingADSBposition.html
-	[[26825, 26641], [36192, 128497], 1]  # This doesn't decode properly. It's on the wrong side of the world, should be off the coast of Florida near -80.xxx, 24-25.xxx
+	[[24745, 21412], [15637, 50148], 1]  # This doesn't decode properly. It's on the wrong side of the world, should be off the coast of Florida near -80.xxx, 24-25.xxx
 ]
 
 # Debug for our mental heath.

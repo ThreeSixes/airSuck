0e268b28f3e327c1882a90a982825479732be2a9
ThreeSixes <actual@joshlucy.com>
2015-02-09 22:04:51 -0500

    Updated state engine to collect low-hanging fruit data types. All data types that require no processing have been added, and support for emergency messages has been set up.

--- ssrStateEngine.py
+++ ssrStateEngine.py
@@ -48,15 +48,16 @@ class SubListener(threading.Thread):
         Returns all data in the cache.
         """
         
-        # Get our time
-        thisTime = str(datetime.datetime.utcnow())
-        
         # Create properly-formatted name for the state hash table we're creating.
         fullName = str('state:' + objName)
         
-        # Do we have a firstSeen key?
-        if self.redis.hsetnx(fullName, 'firstSeen', thisTime) == 0:
-            self.redis.hset(fullName, 'lastSeen', thisTime)
+        # Delete the original timestamp.
+        thisTime = cacheData.pop('dts', None)
+        
+        # Set the first seen data.
+        self.redis.hsetnx(fullName, 'firstSeen', thisTime)
+        # Set last seen either way.
+        self.redis.hset(fullName, 'lastSeen', thisTime)
         
         # Update or create cached data, if we have more than just a name
         if type(cacheData) == dict:
@@ -81,7 +82,8 @@ class SubListener(threading.Thread):
         Put status data on a queue for processing
         """
         
-        pprint(statusData)
+        #Debug print instead of dumping data onto another queue.
+        print(json.dumps(statusData))
         
         return
 
@@ -95,14 +97,39 @@ class SubListener(threading.Thread):
         # Make sure we got good data from json.loads
         if (type(ssrWrapped) == dict):
             
+            # Set up our data structure
+            data = {}
+            
+            # Set our datetime stamp for this data.
+            data.update({"dts": ssrWrapped['dts']})
+            
+            # Automatically grab any emergency data we have ahead of time.
+            if 'emergency' in ssrWrapped:
+                # Inherit the emergency flag from anything with the emergency flag from ANY frame.
+                data.update({'emergency': ssrWrapped['emergency']})
+                
+                # Get a mode A squawk emergency description if one exists.
+                if 'aSquawkEmergency' in ssrWrapped:
+                    data.update({'emergencyData': ssrWrapped['aSquawkEmergency']})
+                
+                # If we have more specific emergency data from an extended status squitter use it instead of a gneeric mode A squawk message.
+                if 'es' in ssrWrapped:
+                    esText = ["No emergency", # This really shouldn't come through without emergency = True.
+                        "General emergency (sqwk 7700)",
+                        "Lifeguard/Medical",
+                        "Minimum Fuel",
+                        "No comms (sqwk 7600)",
+                        "Unlawful interference (sqwk 7500)",
+                        "Downed aircraft",
+                        "Reserved"]
+                    
+                    data.update({'emergencyData': esText[ssrWrapped['es']]})
+            
             # Do we hvae mode s?
             if ssrWrapped['mode'] == "s":
                 
                 # Do we have data we care about?
                 if ssrWrapped['df'] == 11:
-                    
-                    # Filter for the data we need:
-                    data = {'df': 11}
                     
                     # Enqueue processed state data.
                     self.enqueueData(self.updateState(ssrWrapped['icaoAAHx'], data))
@@ -110,7 +137,53 @@ class SubListener(threading.Thread):
                 elif ssrWrapped['df'] == 17:
                     
                     # Filter for the data we need:
-                    data = {'df': 17}
+                    # Mode A squawk code.
+                    if 'aSquawk' in ssrWrapped:
+                        data.update({"aSquawk": ssrWrapped['aSquawk']})
+                    
+                    # ID data
+                    if 'idInfo' in ssrWrapped:
+                        data.update({"idInfo": ssrWrapped['idInfo']})
+                    
+                    # Aircraft category
+                    if 'category' in ssrWrapped:
+                        data.update({"category": ssrWrapped['category']})
+                    
+                    # Aircraft category
+                    if 'heading' in ssrWrapped:
+                        data.update({"heading": ssrWrapped['heading']})
+                    
+                    # Altitude
+                    if 'alt' in ssrWrapped:
+                        data.update({"alt": ssrWrapped['alt']})
+                    
+                    # Vertical rate data
+                    if 'vertRate' in ssrWrapped:
+                        data.update({"vertRate": ssrWrapped['vertRate']})
+                    
+                    # Velo
+                    # Needs some more logic.
+                    
+                    # Vertical status
+                    # Needs moe logic
+                    
+                    # Supersonic?
+                    if 'supersonic' in ssrWrapped:
+                        data.update({"supersonic": ssrWrapped['supersonic']})
+                    
+                    # Surveillance status
+                    if 'ss' in ssrWrapped:
+                        data.update({"survStat": ssrWrapped['ss']})
+                    
+                    # Work on location data.
+                    # This needs a decent amount of logic
+                    
+                    # UTC flag
+                    if 'utc' in ssrWrapped:
+                        data.update({"utc": ssrWrapped['utc']})
+                    
+                    # Handle location data
+                    # This needs a lot of logic.
                     
                     # Enqueue processed state data.
                     self.enqueueData(self.updateState(ssrWrapped['icaoAAHx'], data))
@@ -118,7 +191,7 @@ class SubListener(threading.Thread):
             elif (ssrWrapped['mode'] == "ac") and ('emergency' in ssrWrapped):
                 
                 # Enqueue processed state data.
-                self.enqueueData(self.updateState('A-' + ssrWrapped['aSquawk'], False))
+                self.enqueueData(self.updateState('A-' + ssrWrapped['aSquawk'], data))
             
             # Get the hex data as a string
             #pprint(ssrWrapped)

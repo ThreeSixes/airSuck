93f5f8fa47554d58232a86a673c6b0f8437450ad
ThreeSixes <actual@joshlucy.com>
2015-08-01 13:13:29 -0700

    Update cprMath.py to use floats rather than ints.

--- cprMath.py
+++ cprMath.py
@@ -51,65 +51,65 @@ class cprMath():
             if self.debugOn: print(" NL() lat < 0 lat = abs(lat) = " + str(lat))
         
         # Latitude transition table.
-        if lat < 10.47047130: retVal = 59
-        elif lat < 14.82817437: retVal = 58
-        elif lat < 18.18626357: retVal = 57
-        elif lat < 21.02939493: retVal = 56
-        elif lat < 23.54504487: retVal = 55
-        elif lat < 25.82924707: retVal = 54
-        elif lat < 27.93898710: retVal = 53
-        elif lat < 29.91135686: retVal = 52
-        elif lat < 31.77209708: retVal = 51
-        elif lat < 33.53993436: retVal = 50
-        elif lat < 35.22899598: retVal = 49
-        elif lat < 36.85025108: retVal = 48
-        elif lat < 38.41241892: retVal = 47
-        elif lat < 39.92256684: retVal = 46
-        elif lat < 41.38651832: retVal = 45
-        elif lat < 42.80914012: retVal = 44
-        elif lat < 44.19454951: retVal = 43
-        elif lat < 45.54626723: retVal = 42
-        elif lat < 46.86733252: retVal = 41
-        elif lat < 48.16039128: retVal = 40
-        elif lat < 49.42776439: retVal = 39
-        elif lat < 50.67150166: retVal = 38
-        elif lat < 51.89342469: retVal = 37
-        elif lat < 53.09516153: retVal = 36
-        elif lat < 54.27817472: retVal = 35
-        elif lat < 55.44378444: retVal = 34
-        elif lat < 56.59318756: retVal = 33
-        elif lat < 57.72747354: retVal = 32
-        elif lat < 58.84763776: retVal = 31
-        elif lat < 59.95459277: retVal = 30
-        elif lat < 61.04917774: retVal = 29
-        elif lat < 62.13216659: retVal = 28
-        elif lat < 63.20427479: retVal = 27
-        elif lat < 64.26616523: retVal = 26
-        elif lat < 65.31845310: retVal = 25
-        elif lat < 66.36171008: retVal = 24
-        elif lat < 67.39646774: retVal = 23
-        elif lat < 68.42322022: retVal = 22
-        elif lat < 69.44242631: retVal = 21
-        elif lat < 70.45451075: retVal = 20
-        elif lat < 71.45986473: retVal = 19
-        elif lat < 72.45884545: retVal = 18
-        elif lat < 73.45177442: retVal = 17
-        elif lat < 74.43893416: retVal = 16
-        elif lat < 75.42056257: retVal = 15
-        elif lat < 76.39684391: retVal = 14
-        elif lat < 77.36789461: retVal = 13
-        elif lat < 78.33374083: retVal = 12
-        elif lat < 79.29428225: retVal = 11
-        elif lat < 80.24923213: retVal = 10
-        elif lat < 81.19801349: retVal = 9
-        elif lat < 82.13956981: retVal = 8
-        elif lat < 83.07199445: retVal = 7
-        elif lat < 83.99173563: retVal = 6
-        elif lat < 84.89166191: retVal = 5
-        elif lat < 85.75541621: retVal = 4
-        elif lat < 86.53536998: retVal = 3
-        elif lat < 87.00000000: retVal = 2
-        else: retVal = 1
+        if lat < 10.47047130: retVal = 59.0
+        elif lat < 14.82817437: retVal = 58.0
+        elif lat < 18.18626357: retVal = 57.0
+        elif lat < 21.02939493: retVal = 56.0
+        elif lat < 23.54504487: retVal = 55.0
+        elif lat < 25.82924707: retVal = 54.0
+        elif lat < 27.93898710: retVal = 53.0
+        elif lat < 29.91135686: retVal = 52.0
+        elif lat < 31.77209708: retVal = 51.0
+        elif lat < 33.53993436: retVal = 50.0
+        elif lat < 35.22899598: retVal = 49.0
+        elif lat < 36.85025108: retVal = 48.0
+        elif lat < 38.41241892: retVal = 47.0
+        elif lat < 39.92256684: retVal = 46.0
+        elif lat < 41.38651832: retVal = 45.0
+        elif lat < 42.80914012: retVal = 44.0
+        elif lat < 44.19454951: retVal = 43.0
+        elif lat < 45.54626723: retVal = 42.0
+        elif lat < 46.86733252: retVal = 41.0
+        elif lat < 48.16039128: retVal = 40.0
+        elif lat < 49.42776439: retVal = 39.0
+        elif lat < 50.67150166: retVal = 38.0
+        elif lat < 51.89342469: retVal = 37.0
+        elif lat < 53.09516153: retVal = 36.0
+        elif lat < 54.27817472: retVal = 35.0
+        elif lat < 55.44378444: retVal = 34.0
+        elif lat < 56.59318756: retVal = 33.0
+        elif lat < 57.72747354: retVal = 32.0
+        elif lat < 58.84763776: retVal = 31.0
+        elif lat < 59.95459277: retVal = 30.0
+        elif lat < 61.04917774: retVal = 29.0
+        elif lat < 62.13216659: retVal = 28.0
+        elif lat < 63.20427479: retVal = 27.0
+        elif lat < 64.26616523: retVal = 26.0
+        elif lat < 65.31845310: retVal = 25.0
+        elif lat < 66.36171008: retVal = 24.0
+        elif lat < 67.39646774: retVal = 23.0
+        elif lat < 68.42322022: retVal = 22.0
+        elif lat < 69.44242631: retVal = 21.0
+        elif lat < 70.45451075: retVal = 20.0
+        elif lat < 71.45986473: retVal = 19.0
+        elif lat < 72.45884545: retVal = 18.0
+        elif lat < 73.45177442: retVal = 17.0
+        elif lat < 74.43893416: retVal = 16.0
+        elif lat < 75.42056257: retVal = 15.0
+        elif lat < 76.39684391: retVal = 14.0
+        elif lat < 77.36789461: retVal = 13.0
+        elif lat < 78.33374083: retVal = 12.0
+        elif lat < 79.29428225: retVal = 11.0
+        elif lat < 80.24923213: retVal = 10.0
+        elif lat < 81.19801349: retVal = 9.0
+        elif lat < 82.13956981: retVal = 8.0
+        elif lat < 83.07199445: retVal = 7.0
+        elif lat < 83.99173563: retVal = 6.0
+        elif lat < 84.89166191: retVal = 5.0
+        elif lat < 85.75541621: retVal = 4.0
+        elif lat < 86.53536998: retVal = 3.0
+        elif lat < 87.00000000: retVal = 2.0
+        else: retVal = 1.0
         
         if self.debugOn: print(" NL() returning " + str(retVal))
         
@@ -208,14 +208,14 @@ class cprMath():
             retVal = [0, 0]
             
             # Compute N(i)
-            ni = self.NL(lastFmt) - 1
+            ni = self.NL(lastFmt) - 1.0
             
             if self.debugOn: print(" decodeCPR() N(i) = " + str(ni))
             
             # If N(i) < 1, we should set it to 1.
             if ni < 1:
                 if self.debugOn: print(" decodeCPR() N(i) < 1, N(i) = 1")
-                ni = 1
+                ni = 1.0
             
             # Get dlon
             dlon = 360.0 / ni
@@ -223,7 +223,7 @@ class cprMath():
             if self.debugOn: print(" decodeCPR() dlon = " + str(dlon))
             
             # Get longitude index.
-            m = math.floor((((evenData[1] * (self.NL(lastFmt) - 1)) - (oddData[1] * self.NL(lastFmt))) / 131072.0) + 0.5)
+            m = math.floor((((evenData[1] * (self.NL(lastFmt) - 1.0)) - (oddData[1] * self.NL(lastFmt))) / 131072.0) + 0.5)
             
             if self.debugOn: print(" decodeCPR() m = " + str(m))
             
@@ -237,7 +237,7 @@ class cprMath():
             # Adjust for E/W.
             if retVal[1] > 180:
                 if self.debugOn: print(" decodeCPR() E\W hemisphere compensation - retVal[1] = " + str(retVal[1]))
-                retVal[1] = retVal[1] - 360
+                retVal[1] = retVal[1] - 360.0
             
             if self.debugOn:
                 print(" decodeCPR() Lat = " + str(retVal[0]))

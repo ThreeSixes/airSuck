ddaf33bdd23d7190a154fdaf60d78acf843f73c1
ThreeSixes <actual@joshlucy.com>
2015-08-07 21:19:40 -0700

    Modify format of data to include mode A squawk codes and general formatting.

--- stateSub2Loc.py
+++ stateSub2Loc.py
@@ -42,30 +42,44 @@ class SubListener(threading.Thread):
             locStr = ""
             
             if 'lat' in stateWrapped:
+                # If we have ID data for the flight put it in.
                 if 'idInfo' in stateWrapped:
-                    locStr = stateWrapped['idInfo'] + " [" + stateWrapped['addr'] + "]: "
-                else:
-                    locStr = "[" + stateWrapped['addr'] + "]: "
-                    
+                    locStr = stateWrapped['idInfo'] + " "
+                
+                # If we know the aircraft's squawk code add it, too.
+                if 'aSquawk' in stateWrapped:
+                    locStr = locStr + "(" + stateWrapped['aSquawk'] + ")" 
+                
+                # Add ICAO AA address.
+                locStr = locStr + "[" + stateWrapped['addr'] + "]: "
+                
+                # Add coordinates.
                 locStr = locStr + str(stateWrapped['lat']) + ", " + str(stateWrapped['lon'])
                 
+                # If we know the altitude put it in, too.
                 if 'alt' in stateWrapped:
                     locStr = locStr + " @ " + str(stateWrapped['alt']) + "ft"
-                    
+                
+                # If we know the vertical rate put it in as well.
                 if 'vertRate' in stateWrapped:
                     signExtra = ""
                     
+                    # Determine sign.
                     if stateWrapped['vertRate'] > 0:
                         signExtra = "+"
-                        
+                    
+                    # Add sign and unit to string.
                     locStr = locStr + " (" + signExtra + str(stateWrapped['vertRate']) + "ft/min)"
                 
+                # Put in the heading if we know it.
                 if 'heading' in stateWrapped:
                     locStr = locStr + " - " + str(stateWrapped['heading']) + " deg"
                 
+                # Add aircraft category if we know it.
                 if 'category' in stateWrapped:
-                    locStr = locStr + " ; cat " + stateWrapped['category']
+                    locStr = locStr + "; cat " + stateWrapped['category']
                 
+                # Add vertical status if we know it (air/ground)
                 if 'vertStat' in stateWrapped:
                     locStr = locStr + " (" + stateWrapped['vertStat'] + ")"
                 

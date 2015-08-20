7b2d9f46cdb6906f692850e6d2d73666fa214c9f
ThreeSixes <actual@joshlucy.com>
2015-08-20 12:53:10 -0700

    Move cprTest.py to cprMathTest.py for consistency.

---
+++ cprMathTest.py
@@ -0,0 +1,28 @@
+
+"""
+cprTest by ThreeSixes (https://github.com/ThreeSixes)
+
+This project is licensed under GPLv3. See COPYING for dtails.
+
+This file is part of the airSuck project (https://github.com/ThreeSixes/airSUck).
+"""
+
+from cprMath import cprMath
+
+cprDecoder = cprMath()
+
+surface = False
+dataPoints = [
+	# Formatted in [[even lat, even lon], [odd lat, odd lon], last format]
+	[[92095, 39846], [88385, 125818], 1], # This decodes correctly, from http://www.lll.lu/~edward/edward/adsb/DecodingADSBposition.html
+	[[24745, 21412], [15637, 50148], 1]  # This doesn't decode properly. It's on the wrong side of the world, should be off the coast of Florida near -80.xxx, 24-25.xxx
+]
+
+for thisPoint in dataPoints:
+	coords = cprDecoder.cprResolveGlobal(thisPoint[0], thisPoint[1], thisPoint[2], surface)
+	if coords != False:
+		print(str(coords[0]) + ", " + str(coords[1]))
+	else:
+		print("Bad data.")
+		
+		

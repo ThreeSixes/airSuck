aa72b43884fe9afda6117ce07af054163c226cc3
ThreeSixes <actual@joshlucy.com>
2015-09-02 21:22:24 -0700

    Updated config.py to include a more sane config statement order and more options.

--- config/config.py
+++ config/config.py
@@ -3,21 +3,25 @@ config.py by ThreeSixes (https://github.com/ThreeSixes)
 
 This project is licensed under GPLv3. See COPYING for dtails.
 
-This is the configuation file for the airSuck daemons and clients, except stateNode.js.
+This is the configuation file for the airSuck daemons and global client options for queues, except stateNode.js.
 
 This file is part of the airSuck project (https://github.com/ThreeSixes/airSUck).
 """
 # The Redis services can be on a single server or multiple servers. The queues are broken out like this for flexibility.
 
-# Connector Redis queue server settings
+# Connector Redis queue settings - used by multliple scripts.
 connRel = {host: "<insert hostname here>", port: 6379, reliableQ: "airSuckConnRel"};
-connPub = {host: "<insert hostname here>", port: 6379, reliableQ: "airSuckConnPub"};
+connPub = {host: "<insert hostname here>", port: 6379, PubSubQ: "airSuckConnPub"};
 
-# State engine Redis queue servers settings
+# State engine Redis queue settings - used by multiple scripts.
 stateRel = {host: "<insert hostname here>", port: 6379, reliableQ: "airSuckStateRel"};
-statePub = {host: "<insert hostname here>", port: 6379, reliableQ: "airSuckStatePub"};
+statePub = {host: "<insert hostname here>", port: 6379, PubSubQ: "airSuckStatePub"};
+
 
 # These databases can be stored on the same MongoDB server. The config is broken out for flexibility.
+
+# SSR State engine settings
+ssrStateEngine = {expireTime: 300, cprExpireSec: 20};
 
 # mongoDump settings
 connMongoDB = {host: "<insert hostname here>", port: 1, dbName: "airSuck", coll: "airSSR"};

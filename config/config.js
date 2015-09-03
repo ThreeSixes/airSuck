1a8551eacb43b05e9863e16a061f5823b86f1297
ThreeSixes <actual@joshlucy.com>
2015-09-02 20:24:12 -0700

    Added an example config.js and a README describing the config files and their options.

--- config/config.js
+++ config/config.js
@@ -1,14 +1,17 @@
 //  config.js by ThreeSixes (https://github.com/ThreeSixes)
 //
+//  This file serves as a config file for stateNode.js.
+//
 //  This project is licensed under GPLv3. See COPYING for dtails.
 //
 //  This file is part of the airSuck project (https://github.com/ThreeSixes/airSUck).
 
 // Configuration elements
 exports.getConfig = function() {
-    return { webPort: 8090, // Port we want node.js to serve HTTP on.
-        redisPort: 6379, // Redis TCP port
+    return {
+        webPort: 8090, // Port we want node.js to serve HTTP on.
         redisHost: "<hostname here>", // Redis host with the state pub/sub queue.
+        redisPort: 6379, // Redis TCP port
         redisQueue: 'airStateFeed' // Name of the pub/sub queue.
-        };
+    };
 }

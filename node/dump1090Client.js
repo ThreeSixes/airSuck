f196b2036e5d017446753286d1dbba5664d3c870
ThreeSixes <actual@joshlucy.com>
2015-09-16 16:26:19 -0700

    Add support for sending TCP data to a remote host.

--- node/dump1090Client.js
+++ node/dump1090Client.js
@@ -11,7 +11,10 @@ var config = cfg.getConfig();
 // Set up our needed libraries.
 var net = require('net');
 var d1090 = new net.Socket();
-//var io = require('socket.io')(http);
+var dConn = new net.Socket();
+
+// Vars
+var dConnConnected = false;
 
 // Log event.
 function log(eventText) {
@@ -34,7 +37,14 @@ function handleMessage(message) {
             data = JSON.stringify(data)
             
             // Log the frmae for debugging.
-            log("Frame " + data);
+            //log("Load frame: " + data);
+            
+            // If we're connected to the connector server...
+            if (dConnConnected) {
+                // Send the data to our connector instance. 
+                dConn.write(data + "\n");
+                log("Send frame: " + data);
+            }
         }
     }
 }
@@ -43,7 +53,16 @@ function handleMessage(message) {
 function connect2Dump1090() {
     // Connect up, log connection success.
     d1090.connect(config.client1090.dump1090Port, config.client1090.dump1090Host, function() {
-        log('Connected to ' + config.client1090.dump1090Host + ':' + config.client1090.dump1090Port);
+        log('Connected to dump1090 instance at ' + config.client1090.dump1090Host + ':' + config.client1090.dump1090Port);
+    });
+}
+
+// Connect to our destination dump1090 connector instance to send JSON data.
+function connect2Connector() {
+    // Connect up, log connection success.
+    dConn.connect(config.client1090.connPort, config.client1090.connHost, function() {
+        log('Connected to dump1090 connector at ' + config.client1090.connHost + ':' + config.client1090.connPort);
+        dConnConnected = true;
     });
 }
 
@@ -76,10 +95,44 @@ d1090.on('data', function(data) {
 // When the connection is closed...
 d1090.on('close', function() {
     log('Dump1090 connection to ' + config.client1090.dump1090Host + ':' + config.client1090.dump1090Port + ' closed');
+    
+    // Attempt reconnect.
+    connect2Dump1090();
 });
+
+// When we get data...
+dConn.on('error', function(err) {
+    // Puke error message out.
+    log("Dump1090 connector socket " + err);
+    
+    dConn.destroy();
+    
+    dConnConnected = false;
+    
+    // Find a way to wait for n amount of time.
+    
+    // Attempt reconnect.
+    connect2Connector();
+});
+
+// When we get data from the connector...
+dConn.on('data', function(data) {
+    // Do nothing.
+});
+
+// When the connection is closed...
+dConn.on('close', function() {
+    log('Dump1090 connector connection to ' + config.client1090.connHost + ':' + config.client1090.connPort + ' closed');
+    dConnConnected = false;
+    
+    // Attempt reconnect
+    connect2Connector();
+});
+
 
 // Make the initial attempt to connect, assuming we're enabled.
 if (config.client1090.enabled) {
+    connect2Connector();
     connect2Dump1090();
 } else {
     log("Dump1090 client not enabled in configuration, but executed.")

1a5bd2db33ee6b71f0473e3f17229292b5717e81
ThreeSixes <actual@joshlucy.com>
2015-09-27 21:14:58 -0700

    Fixed dorky error.

--- airSuckUtil.py
+++ airSuckUtil.py
@@ -423,6 +423,8 @@ class airSuckUtil:
             "Other Type, Reserved",
             "Other Type, Reserved",
             "Other Type"]
+        
+        return shipTypes[shipType]
     
     def getEPFDMeta(self, epfd):
         """

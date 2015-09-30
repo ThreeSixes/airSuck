edf53efb01dad01fd8a29fb34a5923448cc166d8
ThreeSixes <actual@joshlucy.com>
2015-09-30 12:20:40 -0700

    Add package file.

---
+++ libAirSuck/__init__.py
@@ -0,0 +1,12 @@
+"""
+libAirSuck by ThreeSixes (https://github.com/ThreeSixes)
+
+This project is licensed under GPLv3. See COPYING for dtails.
+
+This file is part of the airSuck project (https://github.com/ThreeSixes/airSUck).
+"""
+
+from cprMath import cprMath
+from airSuckUtil import airSuckUtil
+from ssrParse import ssrParse
+from aisParse import aisParse

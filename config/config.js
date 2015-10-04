7e5c2685c4f7ccff9aca98c9bace8a2424706f57
ThreeSixes <actual@joshlucy.com>
2015-10-03 19:29:15 -0700

    Make the config look slightly prettier.

--- config/config.js
+++ config/config.js
@@ -16,7 +16,9 @@ exports.getConfig = function() {
             redisHost: "<insert hostname here>", // Redis host with the state pub/sub queue.
             redisPort: 6379, // Redis TCP port
             redisQueue: "airSuckStatePub" // Name of the pub/sub queue.
-        }, client1090: { // Dump1090 client configuration.
+        },
+        
+        client1090: { // Dump1090 client configuration.
             enabled: true, // Even if the process starts do we want the dump1090 client to run?
             srcName: "<insert unique name here>", // Name of source that should appear in the database.
             dump1090Host: "127.0.0.1", // Hostname or IP running the dump1090 service. Defaults is 127.0.0.1
@@ -25,7 +27,7 @@ exports.getConfig = function() {
             connPort: 8091, // Dump1090 connector port
             connectDelay: (5 * 1000), // Global reconnect attempt delay (for the dump1090 process and the destination server)
             connTimeout: 20, // Timeout for a dead connection from the connector server. This should be at least 2x the clientPingInterval in the dump1090 connector config.
-            d1090Timeout: 300 // Timeout for not getting any data from the dump1090 instance. Defaults is 5 mins.
+            d1090Timeout: 300 // Timeout in seconds for not getting any data from the dump1090 instance. Default is 30 mins.
         }
     };
 }

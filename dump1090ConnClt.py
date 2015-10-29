5b3bd90646898bbeca7d7560f9571d499dfdd9ef
ThreeSixes <actual@joshlucy.com>
2015-10-28 23:11:43 -0700

    Refactored exception handing in the dump1090 client completely to prevent multiple connections being opened and improved error checking of incoming data before its queued.

--- dump1090ConnClt.py
+++ dump1090ConnClt.py
@@ -26,7 +26,7 @@ import errno
 import binascii
 import hashlib
 import traceback
-from socket import socket
+import socket
 from pprint import pprint
 from libAirSuck import ssrParse
 
@@ -58,6 +58,10 @@ class dataSource(threading.Thread):
 		self.myName = myName
 		self.dump1090Src = dump1090Src
 		self.__ssrParser = ssrParse()
+		self.__watchdogFail = False
+		
+		# Our class-wide socket object.
+		self.__dump1090Sock = None
 		
 		# This keeps track of the number of seconds since our last connection.
 		self.__lastEntry = 0
@@ -77,27 +81,103 @@ class dataSource(threading.Thread):
 		
 		try:
 			# Check to see if our last entry was inside the timeout window.
-			if self.__lastEntry >= self.dump1090Src['threadTimeout']:
-				# Stop the watchdog.
-				self.__myWatchdog.cancel()
-				
+			#if self.__lastEntry >= self.dump1090Src['threadTimeout']:
+			if self.__lastEntry >= 20:
 				# Puke if we have been running for too long without data.
-				raise IOError(self.myName + ": No data recieved in " + str(self.dump1090Src['threadTimeout']) + " sec. Restarting connection.")
+				raise IOError()
 			else:
 				# Restart our watchdog.
 				self.__myWatchdog = threading.Timer(1.0, self.watchdog)
 				self.__myWatchdog.start()
+		
 		except Exception as e:
-			# Stop the watchdog to avoid spawning more than one.
+			# Set the watchdog failure flag.
+			self.__watchdogFail = True
+			
+			# Stop the watchdog.
 			self.__myWatchdog.cancel()
 			
-			# Dump the exception and restart.
-			tb = traceback.format_exc()
-			print(tb)
-			self.run()
+			# Prion the error message
+			print(self.myName + " watchdog: No data recieved in " + str(self.dump1090Src['threadTimeout']) + " sec.")
+			
+			# Close the connection.
+			self.__dump1090Sock.close()
 		
 		# Increment our last entry.
 		self.__lastEntry += 1
+	
+	# Connect to our data source.
+	def connectSource(self):
+		"""
+		connectSource()
+		
+		Connects to our host.
+		"""
+		
+		# Create a new socket object, and set a 1-second timeout on blocking operations.
+		self.__dump1090Sock = socket.socket()
+		self.__dump1090Sock.settimeout(1.0)
+		
+		# We're not connected so set the flag.
+		notConnected = True
+		
+		# Keep trying to connect until it works.
+		while notConnected:
+			# Print message
+			print(self.myName + " connecting to " + self.dump1090Src["host"] + ":" + str(self.dump1090Src["port"]))
+			
+			# Attempt to connect.
+			try:
+				# Connect up
+				self.__dump1090Sock.connect((self.dump1090Src["host"], self.dump1090Src["port"]))
+				print(self.myName + " connected.")
+				
+				# We connected so now we can move on.
+				notConnected = False
+				
+				# Reset the lastEntry counter.
+				self.__lastEntry = 0
+				
+				# Reset the watchdog state.
+				self.__watchdogFail = False
+				
+			except Exception as e:
+				if 'errno' in e:
+					# If we weren't able to connect, dump a message
+					if e.errno == errno.ECONNREFUSED:
+						#Print some messages
+						print(myName + " failed to connect to " + self.dump1090Src["host"] + ":" + str(self.dump1090Src["port"]))
+				
+				else:
+					# Dafuhq happened!?
+					print(self.myName + " went boom connecting.")
+					tb = traceback.format_exc()
+					print(tb)
+				
+				# In the event our connect fails, try again after the configured delay
+				print(self.myName + " sleeping " + str(self.dump1090Src["reconnectDelay"]) + " sec")
+				time.sleep(self.dump1090Src["reconnectDelay"] * self.backoff)
+	
+	# Disconnect the source and re-create the socket object.
+	def disconnectSouce(self):
+		"""
+		disconnectSource()
+		
+		Disconnect from our host.
+		"""
+		
+		print(self.myName + " disconnecting.")
+		
+		try:	
+			# Close the connection.
+			self.__dump1090Sock.close()
+		except:
+			print(self.myName + " threw exception disconnecting.")
+			tb = traceback.format_exc()
+			print(tb)
+		
+		# Reset the lastEntry counter.
+		self.__lastEntry = 0
 	
 	def run(self):
 		"""run
@@ -109,34 +189,10 @@ class dataSource(threading.Thread):
 		
 		print(myName + " running.")
 		
-		# Create our socket.
-		dump1090Sock = socket()
-		
 		# Do stuff.
-		while (alive):
+		while (True):
 			# Attempt to connect.
-			try:
-				# Connect up
-				print(myName + " connecting to " + dump1090Src["host"] + ":" + str(dump1090Src["port"]))
-				dump1090Sock.connect((dump1090Src["host"], dump1090Src["port"]))
-				print(myName + " connected.")
-				
-			except Exception as e:
-					if 'errno' in e:
-						# If we weren't able to connect, dump a message
-						if e.errno == errno.ECONNREFUSED:
-							
-							#Print some messages
-							print(myName + " failed to connect to " + dump1090Src["host"] + ":" + str(dump1090Src["port"]))
-							print(myName + " sleeping " + str(dump1090Src["reconnectDelay"]) + " sec")
-					else:
-						# Dafuhq happened!?
-						print(self.myName + " went boom.")
-						tb = traceback.format_exc()
-						print(tb)
-					
-					# In the event our connect fails, try again after the configured delay
-					time.sleep(dump1090Src["reconnectDelay"])
+			self.connectSource()
 			
 			# Try to read a lone from our established socket.
 			try:
@@ -146,7 +202,7 @@ class dataSource(threading.Thread):
 				self.__myWatchdog.start()
 				
 				# Get lines of data from dump1090
-				for thisLine in self.readLines(dump1090Sock):
+				for thisLine in self.readLines(self.__dump1090Sock):
 					
 					# Date time string
 					dtsStr = str(datetime.datetime.utcnow())
@@ -169,14 +225,23 @@ class dataSource(threading.Thread):
 						# Split MLAT data from SSR data.
 						lineParts = self.splitMlat(thisLine)
 						
-						# Parse the frame.
-						binData = bytearray(binascii.unhexlify(lineParts[1]))
+						try:
+							# Parse the frame.
+							binData = bytearray(binascii.unhexlify(lineParts[1]))
+						except:
+							# Blank our incoming data and dump an error.
+							binData = ""
+							formattedSSR = ""
+							print(self.myName + " got invlaid hex SSR data: " + lineParts[1])
 						
 						# Set MLAT data and SSR data.
 						thisEntry.update({ 'mlatData': lineParts[0], 'data': lineParts[1] })
 						
 						# Add parsed data.
 						thisEntry.update(self.__ssrParser.ssrParse(binData))
+						
+						# Create an entry to be queued.
+						thisEntry.update({ 'dataOrigin': 'dump1090', 'type': 'airSSR', 'dts': dtsStr, 'src': myName, 'entryPoint': 'dump1090ConnClt' })
 					
 					else:
 						# This gets fed to the deduplicator.
@@ -184,36 +249,52 @@ class dataSource(threading.Thread):
 						
 						# Format our SSR data a hex string and bytearray.
 						formattedSSR = self.formatSSRMsg(thisLine)
-						binData = bytearray(binascii.unhexlify(formattedSSR))
+						
+						try:
+							# Parse the frame.
+							binData = bytearray(binascii.unhexlify(formattedSSR))
+						except:
+							# Blank our incoming data and dump an error.
+							binData = ""
+							formattedSSR = ""
+							print(self.myName + " got invlaid hex SSR data: " + formattedSSR)
 						
 						# Properly format the "line"
 						thisEntry.update({ 'data': formattedSSR })
 						
 						# Parse our data and add it to the stream.
 						thisEntry.update(self.__ssrParser.ssrParse(binData))
-					
-					# Create an entry to be queued.
-					thisEntry.update({ 'dataOrigin': 'dump1090', 'type': 'airSSR', 'dts': dtsStr, 'src': myName, 'entryPoint': 'dump1090ConnClt' })
+						
+						# Create an entry to be queued.
+						thisEntry.update({ 'dataOrigin': 'dump1090', 'type': 'airSSR', 'dts': dtsStr, 'src': myName, 'entryPoint': 'dump1090ConnClt' })
 					
 					# Queue up our data.
 					self.queueADSB(thisEntry, dedupeFlag)
 				
 				# Close the connection.
-				dump1090Sock.close()
+				self.disconnectSouce()
 				
 			# Try to catch what blew up. This needs to be significantly improved and should result in a delay and another connection attempt.
-			except:
-				# Stop the watchdog to avoid spawning more than one.
-				self.__myWatchdog.cancel()
-				
-				# Dafuhq happened!?
-				print(self.myName + " went boom.")
-				tb = traceback.format_exc()
-				print(tb)
-				
-				# Delete our connection.
-				del dump1090Sock
-				dump1090Sock = socket()
+			except Exception as e:
+				if 'errno' in e:
+					if e.errno == 9:
+						continue
+					else:
+						# Dafuhq happened!?
+						print(self.myName + " went boom processing data.")
+						tb = traceback.format_exc()
+						print(tb)
+						
+						# Close the connection.
+						self.disconnectSouce()
+				else:
+					# Dafuhq happened!?
+					print(self.myName + " went boom processing data.")
+					tb = traceback.format_exc()
+					print(tb)
+					
+					# Close the connection.
+					self.disconnectSouce()
 	
 	def formatSSRMsg(self, strMsg):
 		"""
@@ -273,26 +354,41 @@ class dataSource(threading.Thread):
 				data = sock.recv(recvBuffer)
 				buffer += data
 				
-				while buffer.find(delim) != -1:
+				while buffer.find(delim) != -1:	
 					line, buffer = buffer.split('\n', 1)
 					# Debugging...
 					#print("L: " + str(line) + ", B: " + str(buffer))
 					yield line
 			
+			except socket.timeout:
+				continue
+			
 			except Exception as e:
 				# If we had a disconnect event drop out of the loop.
-				if e.errno == 9:
-					print(self.myName + " disconnected.")
-					data = False
-					raise e
+				if 'errno' in e:
+					if e.errno == 9:
+						print(self.myName + " disconnected.")
+						data = False
+						raise e
+					
+					else:
+						print(self.myName + " choked reading buffer.")
+						tb = traceback.format_exc()
+						print(tb)
+						data = False
+						line = ""
 				else:
 					print(self.myName + " choked reading buffer.")
 					tb = traceback.format_exc()
-					#print(tb)
+					print(tb)
 					data = False
-					#buffer = ""
 					line = ""
 			
+			# See if our watchdog is working.
+			if self.__watchdogFail:
+				print(self.myName + " watchdog terminating readLines.")
+				data = False
+				break
 	
 	# Convert the data we want to send to JSON format.
 	def queueADSB(self, msg, dedupeFlag):
@@ -300,36 +396,39 @@ class dataSource(threading.Thread):
 		
 		Accepts a JSON string and queues it in the Redis database, assuming a duplicate string hasn't been queued within the last n seconds specified in redisQueues['dudeupeTableExp'], and we're not dealing with MLAT data. (dedupeFlag = False prevents dedupliation operations.)
 		"""
-		jsonMsg = self.jsonify(msg)
-		# See if we already have the key in the redis cache, or if we're supposed to dedupe this frame at all.
 		
-		# Set up a hashed version of our data.
-		dHash = "ssr-" + hashlib.md5(msg['data']).hexdigest()
-		
-		if ((self.__dedeupe.exists(dHash) == False) or (dedupeFlag == False)):
-			# Set the key and insert lame value.
-			self.__dedeupe.setex(dHash, config.d1090ConnSettings['dedupeTTLSec'], "X")
+		# If we have something in the data field that's longer than 2 chars...
+		if len(msg['data']) >= 4:
+			jsonMsg = self.jsonify(msg)
+			# See if we already have the key in the redis cache, or if we're supposed to dedupe this frame at all.
 			
-			# If we are configured to use the connector mongoDB forward the traffic to it.
-			if config.connMongo['enabled'] == True:
-				self.__rQ.rpush(config.connRel['qName'], jsonMsg)
+			# Set up a hashed version of our data.
+			dHash = "ssr-" + hashlib.md5(msg['data']).hexdigest()
 			
-			# Put data on the pub/sub queue.
-			self.__psQ.publish(config.connPub['qName'], jsonMsg)
+			if ((self.__dedeupe.exists(dHash) == False) or (dedupeFlag == False)):
+				# Set the key and insert lame value.
+				self.__dedeupe.setex(dHash, config.d1090ConnSettings['dedupeTTLSec'], "X")
+				
+				# If we are configured to use the connector mongoDB forward the traffic to it.
+				if config.connMongo['enabled'] == True:
+					self.__rQ.rpush(config.connRel['qName'], jsonMsg)
+				
+				# Put data on the pub/sub queue.
+				self.__psQ.publish(config.connPub['qName'], jsonMsg)
+				
+				print("Q: " + str(msg['data']))
+				
+			# Reset our lastEntry seconds.
+			self.__lastEntry = 0
 			
-			print("Q: " + str(msg['data']))
-			
-		# Reset our lastEntry seconds.
-		self.__lastEntry = 0
-		
-		return
+			return
 
 #######################
 # Main execution body #
 #######################
 
 # ... and go.
-print("Dump1090 connector starting...")
+print("Dump1090 client connector starting...")
 
 # Threading setup
 threadLock = threading.Lock()

103e4305f349fda780b4ee4d79fdec420b242ac2
ThreeSixes <actual@joshlucy.com>
2015-11-19 12:09:23 -0800

    Updated comments on config file elements.

--- node/js/config.js
+++ node/js/config.js
@@ -13,51 +13,51 @@
 /***************************************************
  * DEBUG
  **************************************************/
-var debug = true;
+var debug = true; // Do we want to have debugging data onscreen and in the console?
 
 /***************************************************
  * VEHICLE CONFIGURATION
  **************************************************/
 // Ships
-var dirShip = "m 0,0 -20,50 40,0 -20,-50"
-var dirShipScl = 0.15
-var ndShip = "m 0,0 -20,20 20,20 20,-20 -20,-20"
-var ndShipScl = 0.21
-var shipActive = "#0000ff"//color of active ship icons (hex)
-var shipInactive = "#000066"//color of nonresponsive ship icons (hex)
-var shipAge = 5 * (60 * 1000);//how long to retain a ship after losing contact (miliseconds)
+var dirShip = "m 0,0 -20,50 40,0 -20,-50" // Path we want to use for AIS targets that we have direction data for.
+var dirShipScl = 0.15 // Scale of the path.
+var ndShip = "m 0,0 -20,20 20,20 20,-20 -20,-20" // Path we want to use for AIS targets that we don't have direction data for.
+var ndShipScl = 0.21 // Scale of the path.
+var shipActive = "#0000ff" // Color of active ship icons (hex)
+var shipInactive = "#000066" // Color of nonresponsive ship icons (hex)
+var shipAge = 5 * (60 * 1000); // How long to retain a ship after losing contact (miliseconds)
 
 // Aircraft
-var dirAircraft = "m 0,0 -20,50 20,-20 20,20 -20,-50"
-var dirAircraftScl = 0.15
-var ndAircraft = "m 15,15 a 15,15 0 1 1 -30,0 15,15 0 1 1 30,0 z"
-var ndAircraftScl = 0.24
-var aircraftActive = "#ff0000"//color of active aircraft icons (hex)
-var aircraftInactive = "#660000"//color of nonresponsive aircraft icons (hex)
-var aircraftAge = 2 * (60 * 1000);//how long to retain a ship after losing contact (miliseconds)
+var dirAircraft = "m 0,0 -20,50 20,-20 20,20 -20,-50" // Path we want to use for ADS-B targets we have direction data for.
+var dirAircraftScl = 0.15 // Scale of the path.
+var ndAircraft = "m 15,15 a 15,15 0 1 1 -30,0 15,15 0 1 1 30,0 z" // Path we want to sue for ADS-B targets we don't have direction data for.
+var ndAircraftScl = 0.24 // Scale of the path.
+var aircraftActive = "#ff0000" // Color of active aircraft icons (hex)
+var aircraftInactive = "#660000" // Color of nonresponsive aircraft icons (hex)
+var aircraftAge = 2 * (60 * 1000); // How long to retain a ship after losing contact (miliseconds)
 
 // General
-var vehExpireCheckInterval = 1 * 1000;//frequency to check for expired vehicles (miliseconds)
+var vehExpireCheckInterval = 1 * 1000; // Frequency to check for expired vehicles (miliseconds)
 
 /***************************************************
  * VEHICLE PATH ALTITUDE COLORING
  **************************************************/
-var minimumAltitude = 0;//altitude to set as the lowest color for the ramp (X,000 feet)
-var maximumAltitude = 45;//altitude to set as the highest color for the ramp (X,000 feet)
-var spectrum = ['aqua','yellow','fuchsia','red'];//colors defining the ramp, low at position [0], high at [3]
+var minimumAltitude = 0; // Altitude to set as the lowest color for the ramp (X,000 feet)
+var maximumAltitude = 45; // Altitude to set as the highest color for the ramp (X,000 feet)
+var spectrum = ['aqua','yellow','fuchsia','red']; // Colors defining the ramp, low at position [0], high at [3]
 
 /***************************************************
  * MAPS GENERAL CONFIGURATION
  **************************************************/
-var useLocation = true;//whether to attempt to determine the user's location
-var defaultLat = 45.555080;//default latitude if useLocation=false or detection fails
-var defaultLng = -122.115890;//default longitude if useLocation=false or detection fails
-var defaultZoom = 9;//default zoom level of the map
+var useLocation = true; // Whether to attempt to determine the user's location via browser
+var defaultLat = 45.555080; // Default latitude if useLocation=false or detection fails
+var defaultLng = -122.115890; // Default longitude if useLocation=false or detection fails
+var defaultZoom = 9; // Default zoom level of the map
 
 /***************************************************
  * MAPS CUSTOM STYLE
  **************************************************/
-var customStyleName = "Monochrome";//name of the custom style for the view toggle
+var customStyleName = "Monochrome"; // Name of the custom style for the view toggle
 var customStyle = [
   {
     "featureType": "landscape.man_made",

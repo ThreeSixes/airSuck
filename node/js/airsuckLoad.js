00e698617ad4aded9bdccf65032e3010fb0d57ee
ThreeSixes <actual@joshlucy.com>
2016-01-05 16:55:55 -0800

    Make the timeout very, very small.

--- node/js/airsuckLoad.js
+++ node/js/airsuckLoad.js
@@ -73,7 +73,7 @@ $.getScript("socket.io/socket.io.js",function(){
                href: '/css/sidebar.css'
             }).appendTo('head');
          });  
-      }, 0.1);
+      }, 0.01);
      
  });
 

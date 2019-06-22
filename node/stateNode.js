ecd1befa46df0f3bac6c58770cea2a1dd7cf8b66
ThreeSixes <joshlucy@gmail.com>
2019-06-21 19:48:45 -0700

    Remove unnecessary spaces.

--- node/stateNode.js
+++ node/stateNode.js
@@ -20,7 +20,7 @@ var client = redis.createClient(config.server.redisPort, config.server.redisHost
 if (config.server.logMode == "syslog") {
   // Load the module.
   var syslog = require('node-syslog');
-  
+
   // Configure the module.
   syslog.init("stateNode.js", syslog.LOG_PID | syslog.LOG_ODELAY, syslog.LOG_DAEMON);
 }
@@ -34,18 +34,18 @@ function log(eventText) {
       //do the thing
       syslog.log(syslog.LOG_NOTICE, eventText);
       break;
-    
+
     // We're logging to the console.
     case "console":
       // Do a console.log().
       console.log(new Date().toISOString().replace(/T/, ' ') + ' - ' + eventText);
       break;
-    
+
     // No logging.
     case "none":
       // Do nothing.
       break;
-    
+
     // IDK?
     default:
       break;
@@ -56,23 +56,23 @@ function log(eventText) {
 app.get('/', function(req, res){
   // Path to index.html.
   var indexFile = __dirname + '/wwwroot/index.html';
-  
+
   // Figure out the browser's UA...
   var ua = req.headers['user-agent'];
   $ = {};
   var indexContents = "";
-  
+
   // Make sure we're not using Firefox. FF requires different infomration for strict mode.
   if (/firefox/i.test(ua)) {
     // Replace script tags with someing FireFox can use.
     // When Firefox's implementation of javascript strict mode matures and joins the rest of the world, we can get rid of this.
     var fs = require('fs');
     var indexContents = "";
-    
+
     indexContents = String(fs.readFileSync(indexFile));
-    
+
     var newContents = indexContents.replace(/type=\"text\/javascript\"/gi, 'type="text/javascript;version=1.7"');
-    
+
     // Set content type and send.
     res.set('Content-Type', 'text/html');
     res.send(newContents);
@@ -87,11 +87,11 @@ app.get('/', function(req, res){
 app.get('/', function(req, res){
   // Path to index.html.
   var indexFile = __dirname + '/wwwroot/index.html';
-  
+
   // Figure out the browser's UA...
   var ua = req.headers['user-agent'],
   $ = {};
-  
+
   // Make sure we're not using Firefox. FF requires different infomration for strict mode.
   if (/firefox/i.test(ua)) {
     // Replace script tags with someing FireFox can use.
@@ -99,7 +99,7 @@ app.get('/', function(req, res){
     var fs = require('fs')
     var result = fs.readFile(__dirname + 'index.html');
     result.replace(/ type="text\/javascript"/g, ' type="text/javascript;version=1.8"');
-    
+
     // Set content type and send.
     res.set('Content-Type', 'text/html');
     res.send(result);
@@ -124,9 +124,9 @@ client.on("message", function (channel, message) {
 client.on("error", function (err) {
   // Log the client error.
   log("Redis client error detected: " + err);
-  
+
   // Find a way to wait.
-  
+
   // Try to subscribe to the queue again.
   subscribe();
 });
@@ -135,7 +135,7 @@ client.on("error", function (err) {
 io.on('connection', function(socket){
   // Log a message to the console
   log("New client @ " + socket.request.connection.remoteAddress)
-  
+
   // If they try to send us something give some generic error message.
   socket.on('message', function(msg){
     socket.emit("message", "{\"error\": \"Yeah, no.\"}");
@@ -146,7 +146,7 @@ io.on('connection', function(socket){
 http.listen(config.server.webPort, function(){
   // Log That we're now listening.
   log('stateNode.js listening on *:' + config.server.webPort);
-  
+
   // Send a keepalive and schedule the next keepalive xmission.
   txKeepalive();
 });
@@ -156,10 +156,10 @@ function txKeepalive() {
   // Create formatted date string.
   dateStr = new Date().toISOString().replace(/T/, ' ');
   dateStr = dateStr.substring(0, dateStr.length - 5);
-  
+
   // Send keepalive message.
   io.emit("message", "{\"keepalive\": \"" + dateStr + "\"}");
-  
+
   // Schedule our keepalive in our specified interval.
   setTimeout(txKeepalive, config.server.keepaliveInterval);
 }
@@ -168,7 +168,7 @@ function txKeepalive() {
 function subscribe() {
   // Connect to our pub/sub queue.
   log('Subscribing to pub/sub queue.');
-  
+
   // Subscribe to the state queue.
   client.subscribe(config.server.redisQueue);
 }
